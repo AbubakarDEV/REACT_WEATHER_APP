6afff1e202a09004e4fe593f84d6754cd038a35a
Abubakar Jilani <abub1156@gmail.com>
2023-09-27 14:14:42 +0500

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b4554a7aa8mshb81c250db81b630p1d771ajsnee2330754b88",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "43f904356c21a016a3af61838eda4d09"; // enter your key from openweather API
